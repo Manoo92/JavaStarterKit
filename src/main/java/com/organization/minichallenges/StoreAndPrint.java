b9269ef6650cdd52026eb1beff0f5383b75c81f7
Manoo92 <manognav908@gmail.com>
2026-01-29 16:16:49 -0500

    print customer information

---
+++ src/main/java/com/organization/minichallenges/StoreAndPrint.java
@@ -0,0 +1,13 @@
+package com.organization.minichallenges;
+
+public class StoreAndPrint {
+    public static void main(String[] args){
+        String name = "Mano";
+        int age = 25;
+        double salary = 50000.90;
+
+        System.out.println("Name = " + name);
+        System.out.println("Age = " + age);
+        System.out.println("Salary = " + salary);
+    }
+}
