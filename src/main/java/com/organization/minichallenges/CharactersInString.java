affc9420e2d294562961cc0be7469b7104ee5187
Manoo92 <manognav908@gmail.com>
2026-01-30 16:03:37 -0500

    print character in string

---
+++ src/main/java/com/organization/minichallenges/CharactersInString.java
@@ -0,0 +1,10 @@
+package com.organization.minichallenges;
+
+public class CharactersInString {
+    static void main() {
+        String s = "Java";
+        for(int i = 0; i < s.length(); i++){
+            System.out.println(s.charAt(i));
+        }
+    }
+}
