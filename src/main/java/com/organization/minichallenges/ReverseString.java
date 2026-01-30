fecfe8a4dd5fc0ca9a13f5239d92b7aa51c05d28
Manoo92 <manognav908@gmail.com>
2026-01-30 16:24:20 -0500

    Reverse String

---
+++ src/main/java/com/organization/minichallenges/ReverseString.java
@@ -0,0 +1,10 @@
+package com.organization.minichallenges;
+
+public class ReverseString {
+    static void main() {
+        String s = "Java";
+        for(int i = 1; i<s.length(); i++){
+            System.out.println(s.charAt(i));
+        }
+    }
+}
