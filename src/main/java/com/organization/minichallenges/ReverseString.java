430184079587a50fef9bf2b6b1cec50df14cb7b7
Manoo92 <manognav908@gmail.com>
2026-02-01 13:24:22 -0500

    update text

--- src/main/java/com/organization/minichallenges/ReverseString.java
+++ src/main/java/com/organization/minichallenges/ReverseString.java
@@ -2,7 +2,7 @@ package com.organization.minichallenges;
 
 public class ReverseString {
     static void main() {
-        String s = "Java";
+        String s = "Hi";
         for(int i = s.length()-1; i>=0; i--){
             System.out.println(s.charAt(i));
         }
