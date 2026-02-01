a4b443945fbb53b36a293d33603cd6ec65ce91e9
Manoo92 <manognav908@gmail.com>
2026-02-01 13:27:12 -0500

    Add largest of two numbers logic

---
+++ src/main/java/com/organization/minichallenges/LargestOfTwoNumbers.java
@@ -0,0 +1,13 @@
+package com.organization.minichallenges;
+
+public class LargestOfTwoNumbers {
+    public static void main(String[] args){
+        int a = 1;
+        int b = 5;
+        if(a > b){
+            System.out.println("Largest number is a : " +a);
+        } else {
+            System.out.println("Largest number is b : " +b);
+        }
+    }
+}
