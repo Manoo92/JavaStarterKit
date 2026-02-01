e7e3ef1ac6aa9fe1ffbd56b580fd5ee7f5df36f3
Manoo92 <manognav908@gmail.com>
2026-02-01 13:27:50 -0500

    Add print - range of numbers

---
+++ src/main/java/com/organization/learn/RangeOFNumbers.java
@@ -0,0 +1,9 @@
+package com.organization.learn;
+
+public class RangeOFNumbers {
+    public static void main(String[] args){
+        for(int i = 1; i<=20; i++){
+            System.out.println(i);
+        }
+    }
+}
