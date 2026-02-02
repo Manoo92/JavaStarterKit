29b0672825f76630969a9b0687522f77ed0fe071
Manoo92 <manognav908@gmail.com>
2026-02-02 12:47:28 -0500

    about strings

---
+++ src/main/java/com/organization/learn/Strings.java
@@ -0,0 +1,11 @@
+package com.organization.learn;
+
+public class Strings {
+    public static void main(String[] args){
+        String s = "Johnson";
+
+        System.out.println("Given String : " +s);
+        System.out.println("Total characters in "+ s + " : " +s.length());
+        System.out.println("Print first character : " +s.charAt(0));
+    }
+}
